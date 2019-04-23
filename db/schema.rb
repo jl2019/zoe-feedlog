31a617a74e7d6bf5399ddce4853e4e116e393675
joga luce <49272045+jl2019@users.noreply.github.com>
2019-04-22 18:58:48 -0700

    0.1.x: generate model: user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_17_170423) do
+ActiveRecord::Schema.define(version: 2019_04_23_013356) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 2019_04_17_170423) do
     t.string "name"
     t.string "status"
     t.string "owner"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
