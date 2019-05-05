c0c8e40f6f47bfa09c8ab8e641566b225a6bab65
joga luce <49272045+jl2019@users.noreply.github.com>
2019-05-05 00:33:31 -0700

    routes: add resources (users,pets,foods,pictures) and get:dashboard#dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'dashboard/index'
-  get 'dashboard/dashboard'
+  resources :users, :pets, :foods, :pictures
+  get '/dashboard', to: 'dashboard#dashboard'
   root to: 'dashboard#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
