10dfeac8af70af72d2d2bc305321623b8dd2da27
joga luce <49272045+jl2019@users.noreply.github.com>
2019-05-11 23:16:28 -0700

    controller: dashboard: refactor authentication to a private method

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,15 +5,20 @@ class DashboardController < ApplicationController
     # if we're logged in just go straight to dashboard
     ## redirect #dashboard if @user.logged_in? or something
     # session[:user] ||= User.find_by_id(params[:user])
-    flash[:notice] = "No user found" if !User.exists?({:id => params[:user]})
-    redirect_to dashboard_path(user: params[:user])
+    if authenticate?
+      flash[:info]   = "You logged in!"
+      redirect_to dashboard_path(user: params[:user])
+    end
   end
 
   # GET /dashboard
   # GET /dashboard.json
   def dashboard
     # horrible user tracking
-    @user = User.last
+    unless authenticate?
+      flash[:alert]   = "no user!"
+      redirect_to root_path and return
+    end
     @user = User.find_by_id(params[:user]) if User.exists?({:id => params[:user]})
     # horrible pet photo default
     @pets = [{:photo => 'https://media.wired.com/photos/59270b3ff3e2356fd800b471/2:1/w_2500,c_limit/DataAttackViralPets.jpg'}]
@@ -22,11 +27,13 @@ class DashboardController < ApplicationController
     respond_to do |format|
       format.html   # dashboard.html.erb
       format.json { # testing out json rendering
-        render json: {
-	  :user => User.all,
-	  :pet  => Pet.all
-	}
+        render json: {:user => User.all}
       }
     end
   end
+
+  private
+    def authenticate?
+      User.exists?({:id => params[:user]})
+    end
 end
